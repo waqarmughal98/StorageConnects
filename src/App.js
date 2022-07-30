1ec03f55d5ae565f0890c1bd096748ca23b59a3b
Waqar Ali <52863555+waqarmughal98@users.noreply.github.com>
2022-07-30 18:26:42 +0500

    up

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <HashRouter basename={process.env.PUBLIC_URL}>
           <AnimatePresence exitBeforeEnter>
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/storageconnects" element={<Home />} />
               <Route path="/active-listings" element={<ActiveListing />} />
               <Route path="/sell-storage" element={<SellStorage />} />
               <Route path="/free-evaluation" element={<Evaluation />} />
